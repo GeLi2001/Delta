368d34543cac2a5035b62b943fa19109df7493a8
GeLi2001 <geli2001@berkeley.edu>
2024-12-30 00:14:40 -0800

    add in react

---
+++ packages/ts/llm-test/examples/testData.ts
@@ -0,0 +1,6 @@
+export const testPrompt =
+  "given the comment check if this comment is approrpiate, comment should not include any curse word";
+
+export const testData = ["I love thsi video", "ji ni tai mei", "nmsl", "wcnm"];
+
+export const expectedOutput = [true, true, false, false];
